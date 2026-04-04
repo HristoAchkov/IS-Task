aedb9f90de109acc9a5f6057756c9e708a832fd7
HristoAchkov <achkovh@yahoo.com>
2026-04-05 00:44:29 +0300

    Created dto models, views and constants.

---
+++ IS-Task/Models/ResponseModels/GetAllCategoriesResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using IS_Task.Shared.Enums;
+
+namespace IS_Task.Models.ResponseModels
+{
+    public class GetAllCategoriesResponseModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public CategoryStatus Status { get; set; }
+    }
+}
---
+++ IS-Task/Shared/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace IS_Task.Shared.Constants
+{
+    public static class DataConstants
+    {
+        public const string ProductNameLength = "Name length must be between 0 and 30.";
+        public const string ProductDescriptionLength = "Description length must be between 0 and 100.";
+        public const string PriceRange = "Price must be greater than 0.";
+
+        public const string CategoryNameLength = "Name length must be between 0 and 20.";
+        public const string CategoryDescriptionLength = "Description length must be between 0 and 100.";
+    }
+}
