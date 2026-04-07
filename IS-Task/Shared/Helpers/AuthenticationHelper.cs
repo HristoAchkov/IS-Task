1f7f580b1be0345e218ababda85c030db35a17e5
HristoAchkov <achkovh@yahoo.com>
2026-04-07 12:00:49 +0300

    Added a Helper class to help with the Guest or User/Admin logic.

---
+++ IS-Task/Shared/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.Core;
+using IS_Task.Shared.Constants;
+using System.Security.Claims;
+
+namespace IS_Task.Shared.Helpers
+{
+    public static class AuthenticationHelper
+    {
+        public static long? GetUserId(ClaimsPrincipal user)
+        {
+            if (!user.Identity.IsAuthenticated)
+                return null;
+            return long.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+
+        public static string? GetCartToken(ClaimsPrincipal user, HttpRequest request, HttpResponse response)
+        {
+            if (user.Identity.IsAuthenticated)
+                return null;
+
+            var token = request.Cookies[DataConstants.CartSessionKey];
+            if (token == null)
+            {
+                token = Guid.NewGuid().ToString();
+                response.Cookies.Append(DataConstants.CartSessionKey, token, new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddDays(2)
+                });
+            }
+            return token;
+        }
+    }
+}
